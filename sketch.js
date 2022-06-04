0a7bbbe05e26adfe9a63cfc9679738a927054cad
eignnx <34199632+eignnx@users.noreply.github.com>
2022-06-04 22:54:18 +0000

    added ui

--- sketch.js
+++ sketch.js
@@ -1,78 +1,28 @@
-const N = 500
-const FRAMERATE = 100
-const SORT_NAME = (
-  // "simple bubblesort"
-  // "better bubblesort"
-  // "simple pivot quicksort" // 392.38
-  "median of 3 quicksort"  // 436.46
-  // "pseudomedian quicksort" // 418.33
-  // "quick mean sort"
-)
-const INIT_METHOD = (
-  // "true random"
-  "perlin noise"
-)
+p5.disableFriendlyErrors = true
+
+const FRAMERATE = 60
+
+let N = 100
 const PERLIN_NOISE_SCALE = 0.06
 
 const SEED = (
-  // null
+  null
   // 1234
-  89234759283745
+  // 89234759283745
 )
+
+const BG_COLOR = "#e8f7ff"
 
 let array
 let sorter
 let score = 0
 
-function setup() {
-  createCanvas(400, 400)
-  
-  if (SEED !== null) {
-    randomSeed(SEED)
-    noiseSeed(SEED)
-  }
-  
-  array = randomArray(N)
-  noStroke()
-  fill(255)
-  sorter = sorts[SORT_NAME](array)
-  frameRate(FRAMERATE)
-  
-}
+const arrSizeInput = document.getElementById("arr-size")
+const playModeBtn = document.getElementById("play-mode-btn")
+const infoSection = document.getElementById("info-section")
 
-function draw() {
-  background("rgb(167,175,206)")
-  const {value, done} = sorter.next()
-  if (done) {
-    noLoop()
-    console.log(`final score = ${nf(100 * score, 2, 2)}`)
-    displayArray(array, {})
-  } else {
-    if (value.hasOwnProperty("score")) {
-      score += value.score
-      delete value.score
-    }
-    displayArray(array, value)
-  }
-}
-
-function displayArray(arr, highlighted) {
-  const w = width / N
-  for (const [idx, ele] of arr.entries()) {
-    fill(255)
-    noStroke()
-    for (const [color, indices] of Object.entries(highlighted)) {
-      if (indices.includes(idx)) {
-        fill(color)
-      }
-    }
-    const x = idx * w
-    const h = ele * height
-    rect(x, height, w, -h)
-    
-    fill(0)
-    rect(x, height-h, w, w)
-  }
+function setMessage(m) {
+  infoSection.textContent = m
 }
 
 let ticks = 0
@@ -86,10 +36,114 @@ const initMethods = {
   "perlin noise": perlinNoise
 }
 
+let selectedSortName = Object.keys(sorts)[0]
+let selectedInitMethod = Object.keys(initMethods)[0]
+
+function initUI() {
+  const algSelect = document.getElementById("alg-select")
+  for (const sortName of Object.keys(sorts)) {
+    const o = document.createElement("option")
+    o.value = sortName
+    o.textContent = sortName
+    algSelect.appendChild(o)
+  }
+  algSelect.onchange = function() {
+    selectedSortName = this.value
+  }
+
+  const initMethodSelect = document.getElementById("init-method-select")
+  for (const initMethodName of Object.keys(initMethods)) {
+    const o = document.createElement("option")
+    o.value = initMethodName
+    o.textContent = initMethodName
+    initMethodSelect.appendChild(o)
+  }
+  initMethodSelect.onchange = function() {
+    selectedInitMethod = this.value
+  }
+
+  playModeBtn.onclick = function() {
+    if (this.textContent === "Start") {
+      this.textContent = "Stop"
+      initSortRun()
+      loop()
+    } else if (this.textContent === "Stop") {
+      this.textContent = "Start"
+      noLoop()
+    }
+  }
+}
+
+function initSortRun() {
+  if (SEED !== null) {
+    randomSeed(SEED)
+    noiseSeed(SEED)
+  }
+
+  score = 0
+  setMessage("")
+  N = +arrSizeInput.value
+  array = randomArray(N)
+  sorter = sorts[selectedSortName](array)
+}
+
+function setup() {
+  createCanvas(400, 400).parent("p5-canvas-container")
+
+  initUI()
+  
+  noStroke()
+  frameRate(FRAMERATE)
+
+  noLoop()
+}
+
+function draw() {
+  background(BG_COLOR)
+  const {value, done} = sorter.next()
+  if (done) {
+    noLoop()
+    playModeBtn.textContent = "Start"
+    setMessage(`final score = ${nf(100 * score, 2, 2)}`)
+    displayArray(array, {})
+  } else {
+    if (value.hasOwnProperty("score")) {
+      score += value.score
+      delete value.score
+    }
+    displayArray(array, value)
+  }
+}
+
+function displayArray(arr, highlighted) {
+  const w = width / N
+  noStroke()
+  for (const [idx, ele] of arr.entries()) {
+    
+    colorMode(HSB, 100)
+    fill((1-ele) * 100, 35, 100)
+    colorMode(RGB)
+    
+    for (const [color, indices] of Object.entries(highlighted)) {
+      if (indices.includes(idx)) {
+        fill(color)
+      }
+    }
+    
+    const x = idx * w
+    const h = ele * height
+    rect(x, height, w, -h)
+    
+    fill(0)
+    rect(x, height-h, w, w)
+  }
+}
+
+
 function randomArray(len) {
   let array = []
-  for (let i = 0; i < N; i++) {
-    array.push(initMethods[INIT_METHOD]())
+  for (let i = 0; i < len; i++) {
+    array.push(initMethods[selectedInitMethod]())
   }
   return array
 }
