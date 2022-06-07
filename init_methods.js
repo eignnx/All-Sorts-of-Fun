8fca71b7e411333588a8ffa469915f83dbdff05e
eignnx <34199632+eignnx@users.noreply.github.com>
2022-06-07 05:49:39 +0000

    performance enhancements, add perlin noise back in

--- init_methods.js
+++ init_methods.js
@@ -1,14 +1,14 @@
 const INIT_METHODS = N => ({
   "true random": () => Math.random(),
-  // "perlin noise": () => perlinNoise(),
+  "perlin noise": () => perlinNoise(),
   "sorted": i => i/N,
   "nearly sorted": i => i/N + 5 * (Math.random() - 0.5) / N,
   "reversed": i => 1 - i/N,
-  "middle peak": i => 1 - 2*abs(i - N/2)/N
+  "middle peak": i => 1 - 2 * Math.abs(i - N/2)/N
 })
 
 let ticks = 0
 function perlinNoise() {
   ticks++
-  // return noise(ticks * PERLIN_NOISE_SCALE)
+  return p.noise(ticks * perlinNoiseScale)
 }
--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+let array = null
+let p = null
+let sorter = null
+let perlinNoiseScale = 0.06
+
 const vueApp = Vue.createApp({
   template: `
 <section>
@@ -46,16 +51,11 @@ const vueApp = Vue.createApp({
   `,
 
   data: () => ({
-    sketch: null,
-    p: null,
-    array: null,
-    sorter: null,
     score: 0,
-    arrSize: 100,
-    n: 100,
-    sortName: "basic bubblesort",
+    arrSize: 1000,
+    n: null,
+    sortName: "simple pivot quicksort",
     initMethod: "true random",
-    perlinNoiseScale: 0.06,
     running: false,
     messages: [],
   }),
@@ -66,12 +66,12 @@ const vueApp = Vue.createApp({
   },
   
   mounted() {
-    const sketch = p => {
+    const sketch = pParam => {
 
       // Let's just hold onto this...
-      this.p = p
+      p = pParam
       
-      const FRAMERATE = 50
+      const FRAMERATE = 60
       const BG_COLOR = "#e8f7ff"
       
       p.setup = () => {
@@ -80,30 +80,31 @@ const vueApp = Vue.createApp({
         p.frameRate(FRAMERATE)
         p.background(BG_COLOR)
         p.noLoop()
+        p.strokeCap(p.SQUARE)
       }
     
       p.draw = () => {
         if (!p.isLooping()) return
         
         p.background(BG_COLOR)
-        const {value, done} = this.sorter.next()
+        const {value, done} = sorter.next()
         
         if (done) {
           this.startStop()
           this.addMessage(`final score = ${100 * this.score}`)
-          this.displayArray(p, this.array, {})
+          this.displayArray(p, array, {})
         } else {
           if (value.hasOwnProperty("score")) {
             this.score += value.score
             delete value.score
           }
-          this.displayArray(p, this.array, value)
+          this.displayArray(p, array, value)
         }
       }
     }
     
-    // p5.disableFriendlyErrors = true
-    this.sketch = new p5(sketch, "p5-canvas-container")
+    p5.disableFriendlyErrors = true
+    new p5(sketch, "p5-canvas-container")
   },
 
   methods: {
@@ -114,48 +115,56 @@ const vueApp = Vue.createApp({
     startStop() {
       if (!this.running) {
         this.initSortRun()
-        this.p.loop()
+        p.loop()
       } else {
-        this.p.noLoop()
+        p.noLoop()
       }
       this.running = !this.running
     },
     
     initSortRun() {
       if (SEED !== null) {
-        this.p.randomSeed(SEED)
-        this.p.noiseSeed(SEED)
+        p.randomSeed(SEED)
+        p.noiseSeed(SEED)
         this.ticks = 0
       }
 
       this.n = this.arrSize
       this.score = 0
       const selectedInitMethod = INIT_METHODS(this.n)[this.initMethod]
-      this.array = randomArray(this.n, selectedInitMethod)
-      this.sorter = SORTS[this.sortName](this.array)
+      array = randomArray(this.n, selectedInitMethod)
+      sorter = SORTS[this.sortName](array)
     },
     
     displayArray(p, arr, highlighted) {
+
+      const colors = {}
+      for (const [c, indices] of Object.entries(highlighted)) {
+        indices.forEach(idx => colors[idx] = p.color(c))
+      }
+      
       const w = p.width / this.n
-      p.noStroke()
+      p.strokeWeight(w)
+      
       for (const [idx, ele] of arr.entries()) {
         
-        p.colorMode(p.HSB, 100)
-        p.fill((1-ele) * 100, 35, 100)
-        p.colorMode(p.RGB)
+        const c = colors[idx]
         
-        for (const [color, indices] of Object.entries(highlighted)) {
-          if (indices.includes(idx)) {
-            p.fill(color)
-          }
+        if (c !== undefined) {
+          p.stroke(c)
+        } else {
+          p.colorMode(p.HSB, 100)
+          p.stroke((1-ele) * 100, 35, 100)
+          p.colorMode(p.RGB)
         }
         
         const x = idx * w
         const h = ele * p.height
-        p.rect(x, p.height, w, -h)
         
-        p.fill(0)
-        p.rect(x, p.height - h, w, w)
+        p.line(x, p.height, x, p.height - h)
+        
+        p.stroke(0)
+        p.point(x, p.height - h)
       }
     }
   },
@@ -170,9 +179,9 @@ const SEED = (
 
 
 function randomArray(len, r) {
-  const array = []
+  const arr = []
   for (let i = 0; i < len; i++) {
-    array.push(r(i))
+    arr.push(r(i))
   }
-  return array
+  return arr
 }
